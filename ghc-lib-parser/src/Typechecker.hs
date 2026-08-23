ff71d7e6410bcfc79f1794b21a54901ce68d6351
Saraqch <quispechoquesara@gmail.com>
2026-08-23 18:22:39 -0400

    feat: normalize path and initialize GHC session with no backend

--- ghc-lib-parser/src/Typechecker.hs
+++ ghc-lib-parser/src/Typechecker.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
 import Control.Exception (SomeException, try)
+import GHC (runGhc, getSessionDynFlags, setSessionDynFlags, backend, noBackend)
+import GHC.Paths (libdir)
+import System.Directory (canonicalizePath)
 
 module Typechecker
   ( FunTypeInfo (..),
@@ -30,4 +33,14 @@ analyzeAndTypecheck targetFile = do
   case result of
     Left err -> return (Left (show err))
     Right report -> return (Right report)
+
+runAnalysis :: FilePath -> IO TypecheckReport
+runAnalysis targetFile = do 
+  canonicalTarget <- canonicalizePath targetFile 
+  runGhc (Just libdir) $ do 
+    dflags <- getSessionDynFlags 
+    let dflags' = dflags { backend = noBackend}
+    _ <- setSessionDynFlags dflags'
+
+    error "Step 3 completed: sesion GHC started"
     
